ded056d45b4c7f55d45e524e2788e88cd8f93a14
Alexander <akolchin@gmail.com>
2017-11-08 13:48:26 +0300

    Further tests with GAE servlet

--- src/main/java/agency/akcom/mmg/sherlock/CollectServlet.java
+++ src/main/java/agency/akcom/mmg/sherlock/CollectServlet.java
@@ -1,7 +1,13 @@
 package agency.akcom.mmg.sherlock;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,30 +20,79 @@ public class CollectServlet extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		req.getAttributeNames();
 		
-		System.out.println("GET collect servlet: ");
-		Enumeration<String> attributeNames = req.getAttributeNames();
-		while (attributeNames.hasMoreElements())
-			System.out.println(attributeNames.nextElement());
-		
-		Enumeration<String> headerNames = req.getHeaderNames();
-		while (headerNames.hasMoreElements())
-			System.out.println(headerNames.nextElement());
+		printRequest(req, "GET");
 			
 		resp.setContentType("text/plain");
 		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().print("GET collect servlet");
+		
 
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		printRequest(req, "POST");
+		
 		resp.setContentType("text/plain");
 		resp.setCharacterEncoding("UTF-8");
-		
-		System.out.println("POST collect servlet");
 		resp.getWriter().print("POST collect servlet");
 	}
+	
+	private void printRequest(HttpServletRequest req, String type) throws IOException {
+		System.out.println();
+		System.out.println(type + " collect servlet: ");
+		
+		System.out.println("---");
+		Map<String, String[]> parameterMap = req.getParameterMap();
+		
+		for (Entry<String, String[]> entry : parameterMap.entrySet()) {
+			System.out.println(entry.getKey() + ": " + Arrays.toString(entry.getValue()));
+		}
+		
+		System.out.println("---");
+		Enumeration<String> headerNames = req.getHeaderNames();
+		while (headerNames.hasMoreElements())
+			System.out.println(headerNames.nextElement());
+		
+		System.out.println("---");
+		String payloadRequest = getBody(req);
+		System.out.println(payloadRequest);
+	}
+	
+	public static String getBody(HttpServletRequest request) throws IOException {
+
+	    String body = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+
+	    try {
+	        InputStream inputStream = request.getInputStream();
+	        if (inputStream != null) {
+	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	            char[] charBuffer = new char[128];
+	            int bytesRead = -1;
+	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+	                stringBuilder.append(charBuffer, 0, bytesRead);
+	            }
+	        } else {
+	            stringBuilder.append("");
+	        }
+	    } catch (IOException ex) {
+	        throw ex;
+	    } finally {
+	        if (bufferedReader != null) {
+	            try {
+	                bufferedReader.close();
+	            } catch (IOException ex) {
+	                throw ex;
+	            }
+	        }
+	    }
+
+	    body = stringBuilder.toString();
+	    return body;
+	}
+
 
 }
