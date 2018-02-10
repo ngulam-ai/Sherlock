8bf594a979a873478e92d324118001ef74965bd9
Danil <dchubiryaev@akolchin.com>
2018-02-10 12:59:12 +0300

    changed logic to find time zone use custom dimensions

--- src/main/java/agency/akcom/mmg/sherlock/CollectServlet.java
+++ src/main/java/agency/akcom/mmg/sherlock/CollectServlet.java
@@ -48,7 +48,7 @@ public class CollectServlet extends HttpServlet {
 	private static final String TOPIC_ID = "sherlock-real-time-ga-hit-data";
 	private static final String PROJECT_ID =  "sherlock-184721"; //"mmg-sandbox"; 
 															// ServiceOptions.getDefaultProjectId();
-	private static final Map<String, ArrayList<String>> MAP_ID_TIMEZONE = new HashMap<String, ArrayList<String>>();
+	private static final Map<String, String> MAP_ID_TIMEZONE = new HashMap<String, String>();
 	
 	TopicName topicName = TopicName.of(PROJECT_ID, TOPIC_ID);
 	// Create a publisher instance with default settings bound to the topic
@@ -110,7 +110,7 @@ public class CollectServlet extends HttpServlet {
 		resp.getWriter().print("POST collect servlet");
 	}
 
-	private JSONObject putParamToJSON(HttpServletRequest req) {
+	protected JSONObject putParamToJSON(HttpServletRequest req) {
 		
 		JSONObject reqJson = requestParamsToJSON(req);
 		
@@ -121,29 +121,8 @@ public class CollectServlet extends HttpServlet {
 		tryToPutOnce(reqJson, "ua", req.getHeader("User-Agent"));
 		tryToPutOnce(reqJson, "__uip", req.getRemoteAddr());
 	
-		// --- date, time and so on
-		// Set timeZone in Calendar
-		String idTimeZone = null;
-		try {
-			String countryAdServer = reqJson.get("cd33").toString();
-			String cityAdServer = reqJson.get("cd30").toString();
-			
-			if(MAP_ID_TIMEZONE.containsKey(countryAdServer)) {
-				List<String> listIdTimeZone = MAP_ID_TIMEZONE.get(countryAdServer);
-				for (int i = 0; i < listIdTimeZone.size() ; i++) {
-					if(listIdTimeZone.get(i).endsWith(cityAdServer)) {
-						idTimeZone = listIdTimeZone.get(i);
-						break;
-					}
-				}
-				if(idTimeZone == null) {
-					idTimeZone = MAP_ID_TIMEZONE.get(countryAdServer).get(0);
-				}
-			}
-		} catch (JSONException e) {
-			idTimeZone = "Europe/Madrid"; // TODO Determine and use Analytics account time zone;
-		}
-		
+		// --- date, time and so on default Europe/Madrid:
+		String idTimeZone = "Europe/Madrid"; // TODO Determine and use Analytics account time zone;
 		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(idTimeZone));
 		calendar.setTime(new Date()); 
 		tryToPutOnce(reqJson, "time", "" + calendar.getTime().getTime()); 
@@ -158,11 +137,21 @@ public class CollectServlet extends HttpServlet {
 		// corrected for daylight savings time.
 		tryToPutOnce(reqJson, "minute", "" + String.format("%02d", calendar.get(Calendar.MINUTE)));
 		// Returns the minutes, between 00 and 59, in the hour.
-
-		// ---------------
-		
+					
+		// Set timeZone use of custom dimensions
+		try {
+			String countryAdServer = reqJson.get("cd33").toString();
+			String cityAdServer = reqJson.get("cd30").toString();
+			
+			if(MAP_ID_TIMEZONE.containsKey(cityAdServer)) {
+				idTimeZone = MAP_ID_TIMEZONE.get(cityAdServer);
+			} else if (MAP_ID_TIMEZONE.containsKey(countryAdServer)) {
+				idTimeZone = MAP_ID_TIMEZONE.get(countryAdServer);				
+			} 
+		} catch (JSONException e) {}
+	
 		//TimeZone
-
+		
 // For get local time in UTC "+01:00"
 //		LocalDateTime dt = LocalDateTime.now();
 //		ZoneId zone = ZoneId.of(idTimeZone);
@@ -170,6 +159,7 @@ public class CollectServlet extends HttpServlet {
 //      ZoneOffset zos = zdt.getOffset();
 //      String formatUTC = dt.atZone(zone).getOffset().getId().replace("Z", "+00:00");
 
+		calendar = Calendar.getInstance(TimeZone.getTimeZone(idTimeZone));
         tryToPutOnce(reqJson, "CD91", "" + calendar.getTimeZone().getID());
 		//LocalTime
 		format.applyPattern("HH:mm:ss.SSS");
@@ -324,17 +314,14 @@ public class CollectServlet extends HttpServlet {
 	
 	public static void createMapTimeZone() {
 		String [] list =  TimeZone.getAvailableIDs();
+
 		for (int i = 0; i < list.length; i++) {
 			String[] id = list[i].split("/");
-			String country = id[0];
-			if(!MAP_ID_TIMEZONE.containsKey(country)) {
-				ArrayList<String> idPerCountry = new ArrayList<String>();
-				for (int j = 0; j < list.length; j++) {
-					if (list[j].startsWith(country)) {
-						idPerCountry.add(list[j]);
-					}
+			
+			for (int j = 0; j < id.length; j++) {
+				if(!MAP_ID_TIMEZONE.containsKey(id[j])) {
+					MAP_ID_TIMEZONE.put(id[j], list[i]);
 				}
-				MAP_ID_TIMEZONE.put(country, idPerCountry);
 			}
 		}
 	}
