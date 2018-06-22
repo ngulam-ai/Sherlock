e5cab19c35a49fe9dc75a1a50867514f53d50aa4
Alexander <akolchin@gmail.com>
2018-06-22 15:01:33 +0300

    Fix stub for Session Costs update task

--- sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/task/SessionCostsUpdateTask.java
+++ sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/task/SessionCostsUpdateTask.java
@@ -1,34 +1,72 @@
 package agency.akcom.mmg.sherlock.ui.server.task;
 
+import java.util.UUID;
+
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
-import com.google.cloud.bigquery.Dataset;
-import com.google.cloud.bigquery.DatasetInfo;
+import com.google.cloud.bigquery.FieldValue;
+import com.google.cloud.bigquery.FieldValueList;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobId;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.TableResult;
 
-public class SessionCostsUpdateTask extends AbstractTask{
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SessionCostsUpdateTask extends AbstractTask {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
-		 // Instantiate a client. If you don't specify credentials when constructing a client, the
-	    // client library will look for credentials in the environment, such as the
-	    // GOOGLE_APPLICATION_CREDENTIALS environment variable.
-	    BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
 
-	    // The name for the new dataset
-	    String datasetName = "my_new_dataset";
+		// Instantiate a client. If you don't specify credentials when constructing a
+		// client, the
+		// client library will look for credentials in the environment, such as the
+		// GOOGLE_APPLICATION_CREDENTIALS environment variable.
+		BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId("sherlock-184721").build().getService();
 
-	    // Prepares a new dataset
-	    Dataset dataset = null;
-	    DatasetInfo datasetInfo = DatasetInfo.newBuilder(datasetName).build();
+		QueryJobConfiguration queryConfig = QueryJobConfiguration
+				.newBuilder("SELECT * FROM `MMG_Streaming.tmpDelta` LIMIT 10")
+				// Use standard SQL syntax for queries.
+				// See: https://cloud.google.com/bigquery/sql-reference/
+				.setUseLegacySql(false).build();
 
-	    // Creates the dataset
-	    dataset = bigquery.create(datasetInfo);
+		// Create a job ID so that we can safely retry.
+		JobId jobId = JobId.of(UUID.randomUUID().toString());
+		Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
 
-	    System.out.printf("Dataset %s created.%n", dataset.getDatasetId().getDataset());
+		try {
+			// Wait for the query to complete.
+			queryJob = queryJob.waitFor();
 
-		
+			// Check for errors
+			if (queryJob == null) {
+				throw new RuntimeException("Job no longer exists");
+			} else if (queryJob.getStatus().getError() != null) {
+				// You can also look at queryJob.getStatus().getExecutionErrors() for all
+				// errors, not just the latest one.
+				throw new RuntimeException(queryJob.getStatus().getError().toString());
+			}
+
+			// Get the results.
+			//QueryResponse response = bigquery.getQueryResults(jobId);
+
+			TableResult result = queryJob.getQueryResults();
+
+			// Print all pages of the results.
+			for (FieldValueList row : result.iterateAll()) {
+				FieldValue sessionIdFieldValue = row.get("sessionId");
+				String sessionId = sessionIdFieldValue.isNull() ? null : sessionIdFieldValue.getStringValue();
+
+				FieldValue clientIdFieldValue = row.get("clientId");
+				String clientId = clientIdFieldValue.isNull() ? null : clientIdFieldValue.getStringValue();
+
+				log.info("{}, {}", sessionId, clientId);
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
 	}
 
 	@Override
