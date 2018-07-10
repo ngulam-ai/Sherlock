b50892fca9c5b849c9325ba4e7a4c740980256d7
Alexander <akolchin@gmail.com>
2018-07-10 16:13:02 +0300

    Refactored

--- sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/task/SessionCostsOneDayUpdateTask.java
+++ sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/task/SessionCostsOneDayUpdateTask.java
@@ -20,21 +20,16 @@ public class SessionCostsOneDayUpdateTask extends AbstractTask {
 		this.dateString = dateString;
 	}
 
-
 	@Override
 	public void run() {
 		if (dateString == null || dateString.isEmpty()) {
-			log.error("Called with invalid 'dateString' parameter (null or empty) " );
+			log.error("Called with invalid 'dateString' parameter (null or empty) ");
 			return;
 		}
 
 		BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId("sherlock-184721").build().getService();
-		
-		String query = " UPDATE `sherlock-184721.MMG_Streaming.sessions_copy_" + dateString + "` AS s";
-		query += " SET trafficSource.adCost = adCostTotal, trafficSource.attributedAdCost = attributedAdCostTotal";
-		query += " FROM `sherlock-184721.MMG_Streaming.daily_sessions_copy_with_cost_increments` AS c";
-		query += " WHERE ((s.sessionId is NULL and c.sessionId is NULL) OR (s.sessionId = c.sessionId)) AND ((s.clientId is NULL and c.clientId is NULL) OR (s.clientId = c.clientId))";
-		query += "       AND c.tableDate = '" + dateString + "'"; 
+
+		String query = String.format(SessionCostsUpdateTask.QUERY_TEMPLATE, dateString);
 
 		QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).setUseLegacySql(false).build();
 
@@ -54,12 +49,11 @@ public class SessionCostsOneDayUpdateTask extends AbstractTask {
 				// errors, not just the latest one.
 				throw new RuntimeException(queryJob.getStatus().getError().toString());
 			}
-			
+
 			log.info("Session costs for day '" + dateString + "' successfully updated");
 		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
-
 
 	}
 
--- sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/task/SessionCostsUpdateTask.java
+++ sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/task/SessionCostsUpdateTask.java
@@ -6,38 +6,45 @@ import java.util.UUID;
 
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
-import com.google.cloud.bigquery.FieldValue;
-import com.google.cloud.bigquery.FieldValueList;
 import com.google.cloud.bigquery.Job;
 import com.google.cloud.bigquery.JobId;
 import com.google.cloud.bigquery.JobInfo;
 import com.google.cloud.bigquery.QueryJobConfiguration;
-import com.google.cloud.bigquery.TableResult;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SessionCostsUpdateTask extends AbstractTask {
 
-	private static final int NUMBER_OF_DAYS_TO_PROCESS_BACK = 11;
+	static final String QUERY_TEMPLATE = " UPDATE `sherlock-184721.MMG_Streaming.sessions_copy_%1$s` AS s"
+			+ " SET trafficSource.adCost = adCostTotal, trafficSource.attributedAdCost = attributedAdCostTotal"
+			+ " FROM `sherlock-184721.MMG_Streaming.daily_sessions_copy_with_cost_increments` AS c"
+	        + " WHERE ((s.sessionId is NULL and c.sessionId is NULL) OR (s.sessionId = c.sessionId)) AND ((s.clientId is NULL and c.clientId is NULL) OR (s.clientId = c.clientId))"
+			+ "       AND c.tableDate = '%1$s'";
+	private static final int NUMBER_OF_DAYS_TO_PROCESS_BACK = 200;
+	private static final LocalDate EARLIEST_POSSIBLE_START_DATE = LocalDate.parse("2018-06-04");
 	
 	@Override 
 	public void run() {
 		// set "dateString" parameter by yesterday date
 		// TODO return back to LocalDate.now().minusDays(1); after tests 
-		LocalDate dayToProcess = LocalDate.parse("2018-07-05"); //LocalDate.now().minusDays(1);
-		for (int count = 0; count < NUMBER_OF_DAYS_TO_PROCESS_BACK; count++) {
+		LocalDate dateToProcess = LocalDate.parse("2018-07-05"); // LocalDate.now().minusDays(1);
+		for (int count = 0; count < NUMBER_OF_DAYS_TO_PROCESS_BACK
+				&& dateToProcess.isAfter(EARLIEST_POSSIBLE_START_DATE.minusDays(1)); count++) {
 
-			String dateString = dayToProcess.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+			String dateString = dateToProcess.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+			
+			// runAllWithinThisTask(dateString) ;
 			
 			SessionCostsOneDayUpdateTask task = new SessionCostsOneDayUpdateTask(dateString);
 			task.enqueue();
 
-			dayToProcess = dayToProcess.minusDays(1); // move one day back
+			dateToProcess = dateToProcess.minusDays(1); // move one day back
 		}
 	}
 
-	public void runAllAtOnce() {
+	private void runAllWithinThisTask(String dateString) {
+
 		// Instantiate a client. If you don't specify credentials when constructing a
 		// client, the
 		// client library will look for credentials in the environment, such as the
@@ -45,61 +52,9 @@ public class SessionCostsUpdateTask extends AbstractTask {
 		//
 		BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId("sherlock-184721").build().getService();
 
-		// set "dateString" parameter by yesterday date
-		LocalDate dayToProcess = LocalDate.parse("2018-07-05"); //LocalDate.now().minusDays(1);
-		for (int count = 0; count < NUMBER_OF_DAYS_TO_PROCESS_BACK; count++) {
+		String query = String.format(QUERY_TEMPLATE, dateString);
 
-			String dateString = dayToProcess.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-
-			String query = " UPDATE `sherlock-184721.MMG_Streaming.sessions_copy_" + dateString + "` AS s";
-			query += " SET trafficSource.adCost = adCostTotal, trafficSource.attributedAdCost = attributedAdCostTotal";
-			query += " FROM `sherlock-184721.MMG_Streaming.daily_sessions_copy_with_cost_increments` AS c";
-			query += " WHERE ((s.sessionId is NULL and c.sessionId is NULL) OR (s.sessionId = c.sessionId)) AND ((s.clientId is NULL and c.clientId is NULL) OR (s.clientId = c.clientId))";
-			query += "       AND c.tableDate = '" + dateString + "'"; 
-
-			QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).setUseLegacySql(false).build();
-
-			// Create a job ID so that we can safely retry.
-			JobId jobId = JobId.of(UUID.randomUUID().toString());
-			Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
-
-			try {
-				// Wait for the query to complete.
-				queryJob = queryJob.waitFor();
-
-				// Check for errors
-				if (queryJob == null) {
-					throw new RuntimeException("Job no longer exists");
-				} else if (queryJob.getStatus().getError() != null) {
-					// You can also look at queryJob.getStatus().getExecutionErrors() for all
-					// errors, not just the latest one.
-					throw new RuntimeException(queryJob.getStatus().getError().toString());
-				}
-				
-				log.info("Session costs for day '" + dateString + "' successfully updated");
-			} catch (Exception e) {
-				log.error(e.getMessage());
-			}
-			
-			
-
-			dayToProcess = dayToProcess.minusDays(1); // move one day back
-		}
-	}
-
-	public void run_() {
-
-		// Instantiate a client. If you don't specify credentials when constructing a
-		// client, the
-		// client library will look for credentials in the environment, such as the
-		// GOOGLE_APPLICATION_CREDENTIALS environment variable.
-		BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId("sherlock-184721").build().getService();
-
-		QueryJobConfiguration queryConfig = QueryJobConfiguration
-				.newBuilder("SELECT * FROM `MMG_Streaming.tmpDelta` LIMIT 10")
-				// Use standard SQL syntax for queries.
-				// See: https://cloud.google.com/bigquery/sql-reference/
-				.setUseLegacySql(false).build();
+		QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).setUseLegacySql(false).build();
 
 		// Create a job ID so that we can safely retry.
 		JobId jobId = JobId.of(UUID.randomUUID().toString());
@@ -118,24 +73,11 @@ public class SessionCostsUpdateTask extends AbstractTask {
 				throw new RuntimeException(queryJob.getStatus().getError().toString());
 			}
 
-			// Get the results.
-			// QueryResponse response = bigquery.getQueryResults(jobId);
-
-			TableResult result = queryJob.getQueryResults();
-
-			// Print all pages of the results.
-			for (FieldValueList row : result.iterateAll()) {
-				FieldValue sessionIdFieldValue = row.get("sessionId");
-				String sessionId = sessionIdFieldValue.isNull() ? null : sessionIdFieldValue.getStringValue();
-
-				FieldValue clientIdFieldValue = row.get("clientId");
-				String clientId = clientIdFieldValue.isNull() ? null : clientIdFieldValue.getStringValue();
-
-				log.info("{}, {}", sessionId, clientId);
-			}
+			log.info("Session costs for day '" + dateString + "' successfully updated");
 		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
+
 	}
 
 	@Override
