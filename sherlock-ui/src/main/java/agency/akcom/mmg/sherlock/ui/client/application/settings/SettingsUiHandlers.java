82d4f0642556ca95c85af18f7ea6e2d958dffac0
SNikitinI <snikitin@akolchin.com>
2018-11-29 13:37:26 +0300

    refactoring SettingsView

--- sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/client/application/settings/SettingsUiHandlers.java
+++ sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/client/application/settings/SettingsUiHandlers.java
@@ -3,8 +3,6 @@ package agency.akcom.mmg.sherlock.ui.client.application.settings;
 import agency.akcom.mmg.sherlock.ui.shared.dto.DspDto;
 import com.gwtplatform.mvp.client.UiHandlers;
 
-import java.util.ArrayList;
-
 public interface SettingsUiHandlers extends UiHandlers {
     void onSaveClick(DspDto dspDto);
 }
