f8c980296ab4bad451e2db4aea670fdf5e5a486e
SNikitinI <snikitin@akolchin.com>
2018-11-30 14:12:53 +0300

    authorization enable

--- sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/dao/CustomerDao.java
+++ sherlock-ui/src/main/java/agency/akcom/mmg/sherlock/ui/server/dao/CustomerDao.java
@@ -7,46 +7,49 @@ import agency.akcom.mmg.sherlock.ui.domain.Customer;
 
 public class CustomerDao extends BaseDao<Customer> {
 
-	public CustomerDao() {
-		super(Customer.class);
-	}
+    public CustomerDao() {
+        super(Customer.class);
+    }
 
-	@Override
-	public Customer saveAndReturn(Customer customer) {
+    @Override
+    public Customer saveAndReturn(Customer customer) {
 
-		AppUserDao appUserDao = new AppUserDao();
-		AppUser appUser;
-		if(customer.getEmail().contains("@ngulam.com"))
-			return null;
+        AppUserDao appUserDao = new AppUserDao();
+        AppUser appUser;
+        if (customer.getEmail().contains("@ngulam.com"))
+            return null;
 
-		if (customer.getUsers().size() == 0) {
-			appUser = new AppUser();
-			appUserDao.save(appUser);
-			customer.getUsers().add(Ref.create(appUser));
-		} else
-			appUser = customer.getUsers().get(0).get();
+        if (customer.getUsers().size() == 0) {
+            appUser = new AppUser();
+            appUserDao.save(appUser);
+            customer.getUsers().add(Ref.create(appUser));
+        } else
+            appUser = customer.getUsers().get(0).get();
 
-		customer.setAdmin(customer.isAdmin());
-		customer.setEmail(customer.getEmail());
-		customer.setPictureURL(customer.getPictureURL());
-		customer = super.saveAndReturn(customer);
+        customer.setAdmin(customer.isAdmin());
+        customer.setEmail(customer.getEmail());
+        customer.setPictureURL(customer.getPictureURL());
+        customer = super.saveAndReturn(customer);
 
-		// should be updated because it can be modified in one of set methods above
-		appUser.setCustomer(customer);
-		// TODO all new USERS will be admins - remove after testing
-		// appUser.setAdmin(true);
-		appUser.setAdmin(false);
-		// ---
-		appUserDao.save(appUser);
+        // should be updated because it can be modified in one of set methods above
+        appUser.setCustomer(customer);
+        // TODO all new USERS will be admins - remove after testing
+        // appUser.setAdmin(true);
+        if (customer.getEmail().contains("@akolchin.com"))
+            appUser.setAdmin(true);
+        else
+            appUser.setAdmin(false);
+        // ---
+        appUserDao.save(appUser);
 
-		return customer;
-	}
+        return customer;
+    }
 
-	@Override
-	public void delete(Customer customer) {
-		AppUserDao appUserDao = new AppUserDao();
-		appUserDao.delete(customer.getUser());
-		super.delete(customer);
-	}
+    @Override
+    public void delete(Customer customer) {
+        AppUserDao appUserDao = new AppUserDao();
+        appUserDao.delete(customer.getUser());
+        super.delete(customer);
+    }
 
 }
